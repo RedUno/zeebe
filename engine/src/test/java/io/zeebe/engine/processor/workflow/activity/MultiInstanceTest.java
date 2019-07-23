b8f0b027b8310cc6753cd9f771fee3e8207735a2
Philipp Ossler <philipp.ossler@gmail.com>
2019-07-23 11:29:58 +0200

    test(engine): clean up

--- engine/src/test/java/io/zeebe/engine/processor/workflow/activity/MultiInstanceTest.java
+++ engine/src/test/java/io/zeebe/engine/processor/workflow/activity/MultiInstanceTest.java
@@ -10,15 +10,16 @@ package io.zeebe.engine.processor.workflow.activity;
 import io.zeebe.engine.util.EngineRule;
 import io.zeebe.model.bpmn.Bpmn;
 import io.zeebe.model.bpmn.BpmnModelInstance;
+import io.zeebe.model.bpmn.builder.MultiInstanceLoopCharacteristicsBuilder;
 import io.zeebe.protocol.record.Assertions;
 import io.zeebe.protocol.record.Record;
 import io.zeebe.protocol.record.intent.IncidentIntent;
 import io.zeebe.protocol.record.intent.JobIntent;
 import io.zeebe.protocol.record.intent.VariableIntent;
 import io.zeebe.protocol.record.intent.WorkflowInstanceIntent;
+import io.zeebe.protocol.record.value.BpmnElementType;
 import io.zeebe.protocol.record.value.ErrorType;
 import io.zeebe.protocol.record.value.IncidentRecordValue;
-import io.zeebe.protocol.record.value.JobBatchRecordValue;
 import io.zeebe.protocol.record.value.WorkflowInstanceRecordValue;
 import io.zeebe.test.util.record.RecordingExporter;
 import io.zeebe.test.util.record.RecordingExporterTestWatcher;
@@ -28,26 +29,34 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
 
 public class MultiInstanceTest {
 
   @ClassRule public static final EngineRule ENGINE = EngineRule.singlePartition();
 
-  public static final String PROCESS_ID = "process";
+  private static final String PROCESS_ID = "process";
+  private static final String ELEMENT_ID = "task";
+  private static final String JOB_TYPE = "test";
+  private static final String INPUT_COLLECTION = "items";
+  private static final String INPUT_ELEMENT = "item";
 
-  private static final BpmnModelInstance WORKFLOW_MI_PARALLEL_SERVICE_TASK =
-      Bpmn.createExecutableProcess(PROCESS_ID)
-          .startEvent()
-          .serviceTask(
-              "task",
-              t ->
-                  t.zeebeTaskType("test")
-                      .multiInstance(
-                          m -> m.zeebeInputCollection("items").zeebeInputElement("item")))
-          .endEvent()
-          .done();
+  private static final BpmnModelInstance WORKFLOW =
+      workflow(b -> b.zeebeInputCollection(INPUT_COLLECTION).zeebeInputElement(INPUT_ELEMENT));
+
+  private static BpmnModelInstance workflow(
+      Consumer<MultiInstanceLoopCharacteristicsBuilder> builder) {
+    return Bpmn.createExecutableProcess(PROCESS_ID)
+        .startEvent()
+        .serviceTask(ELEMENT_ID, t -> t.zeebeTaskType(JOB_TYPE).multiInstance(builder))
+        .endEvent()
+        .done();
+  }
 
   @Rule
   public final RecordingExporterTestWatcher recordingExporterTestWatcher =
@@ -55,50 +64,87 @@ public class MultiInstanceTest {
 
   @Test
   public void shouldCreateOneElementInstanceForEachElement() {
-    ENGINE.deployment().withXmlResource(WORKFLOW_MI_PARALLEL_SERVICE_TASK).deploy();
+    // given
+    ENGINE.deployment().withXmlResource(WORKFLOW).deploy();
 
+    // when
     final long workflowInstanceKey =
         ENGINE
             .workflowInstance()
             .ofBpmnProcessId(PROCESS_ID)
-            .withVariable("items", Arrays.asList(1, 2, 3))
+            .withVariable(INPUT_COLLECTION, Arrays.asList(10, 20, 30))
             .create();
+
+    // then
+    final Record<WorkflowInstanceRecordValue> multiInstanceBody =
+        RecordingExporter.workflowInstanceRecords(WorkflowInstanceIntent.ELEMENT_ACTIVATED)
+            .withWorkflowInstanceKey(workflowInstanceKey)
+            .withElementId(ELEMENT_ID)
+            .getFirst();
+
+    Assertions.assertThat(multiInstanceBody.getValue())
+        .hasBpmnElementType(
+            BpmnElementType.SERVICE_TASK) // TODO (saig0): should have a special type?
+        .hasFlowScopeKey(workflowInstanceKey);
 
     assertThat(
             RecordingExporter.workflowInstanceRecords(WorkflowInstanceIntent.ELEMENT_ACTIVATED)
                 .withWorkflowInstanceKey(workflowInstanceKey)
-                .withElementId("task")
-                .limit(4))
-        .hasSize(4);
+                .withElementId(ELEMENT_ID)
+                .skip(1)
+                .limit(3))
+        .hasSize(3)
+        .extracting(Record::getValue)
+        .extracting(r -> tuple(r.getFlowScopeKey(), r.getBpmnElementType()))
+        .containsOnly(tuple(multiInstanceBody.getKey(), BpmnElementType.SERVICE_TASK));
   }
 
   @Test
-  public void shouldCreateOneTaskForEachElement() {
-    ENGINE.deployment().withXmlResource(WORKFLOW_MI_PARALLEL_SERVICE_TASK).deploy();
+  public void shouldCreateOneJobForEachElement() {
+    // given
+    ENGINE.deployment().withXmlResource(WORKFLOW).deploy();
 
+    // when
     final long workflowInstanceKey =
         ENGINE
             .workflowInstance()
             .ofBpmnProcessId(PROCESS_ID)
-            .withVariable("items", Arrays.asList(1, 2, 3))
+            .withVariable(INPUT_COLLECTION, Arrays.asList(10, 20, 30))
             .create();
+
+    // then
+    final List<Long> elementInstanceKey =
+        RecordingExporter.workflowInstanceRecords(WorkflowInstanceIntent.ELEMENT_ACTIVATED)
+            .withWorkflowInstanceKey(workflowInstanceKey)
+            .withElementId(ELEMENT_ID)
+            .skip(1)
+            .limit(3)
+            .map(Record::getKey)
+            .collect(Collectors.toList());
 
     assertThat(
             RecordingExporter.jobRecords(JobIntent.CREATED)
                 .withWorkflowInstanceKey(workflowInstanceKey)
                 .limit(3))
-        .hasSize(3);
+        .hasSize(3)
+        .extracting(Record::getValue)
+        .extracting(r -> tuple(r.getElementId(), r.getElementInstanceKey()))
+        .containsExactly(
+            tuple(ELEMENT_ID, elementInstanceKey.get(0)),
+            tuple(ELEMENT_ID, elementInstanceKey.get(1)),
+            tuple(ELEMENT_ID, elementInstanceKey.get(2)));
   }
 
   @Test
   public void shouldSetInputElementVariable() {
-    ENGINE.deployment().withXmlResource(WORKFLOW_MI_PARALLEL_SERVICE_TASK).deploy();
+    // given
+    ENGINE.deployment().withXmlResource(WORKFLOW).deploy();
 
     final long workflowInstanceKey =
         ENGINE
             .workflowInstance()
             .ofBpmnProcessId(PROCESS_ID)
-            .withVariable("items", Arrays.asList("a", "b", "c"))
+            .withVariable(INPUT_COLLECTION, Arrays.asList(10, 20, 30))
             .create();
 
     RecordingExporter.jobRecords(JobIntent.CREATED)
@@ -106,35 +152,65 @@ public class MultiInstanceTest {
         .limit(3)
         .exists();
 
-    ENGINE.jobs().withType("test").activate();
+    // when
+    ENGINE.jobs().withType(JOB_TYPE).activate();
 
+    // then
     assertThat(
             RecordingExporter.jobRecords(JobIntent.ACTIVATED)
                 .withWorkflowInstanceKey(workflowInstanceKey)
                 .limit(3))
-        .extracting(r -> r.getValue().getVariables().get("item"))
         .hasSize(3)
-        .contains("a", "b", "c");
-
-    assertThat(
-            RecordingExporter.variableRecords(VariableIntent.CREATED)
-                .withName("item")
-                .withWorkflowInstanceKey(workflowInstanceKey)
-                .limit(3))
-        .hasSize(3)
-        .extracting(r -> r.getValue().getValue())
-        .contains("\"a\"", "\"b\"", "\"c\"");
+        .extracting(r -> r.getValue().getVariables().get(INPUT_ELEMENT))
+        .containsExactly(10, 20, 30);
   }
 
   @Test
-  public void shouldCompleteActivityWhenAllInstancesAreCompleted() {
-    ENGINE.deployment().withXmlResource(WORKFLOW_MI_PARALLEL_SERVICE_TASK).deploy();
+  public void shouldWriteInputElementVariableRecord() {
+    // given
+    ENGINE.deployment().withXmlResource(WORKFLOW).deploy();
+
+    // when
+    final long workflowInstanceKey =
+        ENGINE
+            .workflowInstance()
+            .ofBpmnProcessId(PROCESS_ID)
+            .withVariable(INPUT_COLLECTION, Arrays.asList(10, 20, 30))
+            .create();
+
+    // then
+    final List<Long> elementInstanceKey =
+        RecordingExporter.workflowInstanceRecords(WorkflowInstanceIntent.ELEMENT_ACTIVATED)
+            .withWorkflowInstanceKey(workflowInstanceKey)
+            .withElementId(ELEMENT_ID)
+            .skip(1)
+            .limit(3)
+            .map(Record::getKey)
+            .collect(Collectors.toList());
+
+    assertThat(
+            RecordingExporter.variableRecords()
+                .withWorkflowInstanceKey(workflowInstanceKey)
+                .withName(INPUT_ELEMENT)
+                .limit(3))
+        .hasSize(3)
+        .extracting(r -> tuple(r.getValue().getValue(), r.getValue().getScopeKey(), r.getIntent()))
+        .containsExactly(
+            tuple("10", elementInstanceKey.get(0), VariableIntent.CREATED),
+            tuple("20", elementInstanceKey.get(1), VariableIntent.CREATED),
+            tuple("30", elementInstanceKey.get(2), VariableIntent.CREATED));
+  }
+
+  @Test
+  public void shouldCompleteBodyWhenAllElementInstancesAreCompleted() {
+    // given
+    ENGINE.deployment().withXmlResource(WORKFLOW).deploy();
 
     final long workflowInstanceKey =
         ENGINE
             .workflowInstance()
             .ofBpmnProcessId(PROCESS_ID)
-            .withVariable("items", Arrays.asList("a", "b", "c"))
+            .withVariable(INPUT_COLLECTION, Arrays.asList(10, 20, 30))
             .create();
 
     RecordingExporter.jobRecords(JobIntent.CREATED)
@@ -142,43 +218,63 @@ public class MultiInstanceTest {
         .limit(3)
         .exists();
 
-    final Record<JobBatchRecordValue> activatedRecord = ENGINE.jobs().withType("test").activate();
-
-    activatedRecord
+    // when
+    ENGINE
+        .jobs()
+        .withType(JOB_TYPE)
+        .activate()
         .getValue()
         .getJobKeys()
         .forEach(jobKey -> ENGINE.job().withKey(jobKey).complete());
 
-    assertThat(
-            RecordingExporter.workflowInstanceRecords(WorkflowInstanceIntent.ELEMENT_COMPLETED)
-                .withWorkflowInstanceKey(workflowInstanceKey)
-                .withElementId("task")
-                .limit(4))
-        .hasSize(4);
+    // then
+    final List<Long> elementInstanceKey =
+        RecordingExporter.workflowInstanceRecords(WorkflowInstanceIntent.ELEMENT_ACTIVATED)
+            .withWorkflowInstanceKey(workflowInstanceKey)
+            .withElementId(ELEMENT_ID)
+            .limit(4)
+            .map(Record::getKey)
+            .collect(Collectors.toList());
 
     assertThat(
             RecordingExporter.workflowInstanceRecords(WorkflowInstanceIntent.ELEMENT_COMPLETED)
-                .withRecordKey(workflowInstanceKey)
-                .limitToWorkflowInstanceCompleted()
-                .exists())
-        .isTrue();
+                .withWorkflowInstanceKey(workflowInstanceKey)
+                .withElementId(ELEMENT_ID)
+                .limit(4))
+        .hasSize(4)
+        .extracting(Record::getKey)
+        .containsExactly(
+            elementInstanceKey.get(1),
+            elementInstanceKey.get(2),
+            elementInstanceKey.get(3),
+            elementInstanceKey.get(0));
+
+    assertThat(
+            RecordingExporter.workflowInstanceRecords()
+                .filterRootScope()
+                .limitToWorkflowInstanceCompleted())
+        .extracting(Record::getIntent)
+        .contains(WorkflowInstanceIntent.ELEMENT_COMPLETED);
   }
 
   @Test
   public void shouldSkipIfCollectionIsEmpty() {
-    ENGINE.deployment().withXmlResource(WORKFLOW_MI_PARALLEL_SERVICE_TASK).deploy();
+    // given
+    ENGINE.deployment().withXmlResource(WORKFLOW).deploy();
 
+    // when
     final long workflowInstanceKey =
         ENGINE
             .workflowInstance()
             .ofBpmnProcessId(PROCESS_ID)
-            .withVariable("items", Collections.emptyList())
+            .withVariable(INPUT_COLLECTION, Collections.emptyList())
             .create();
 
+    // then
     assertThat(
             RecordingExporter.workflowInstanceRecords()
                 .withWorkflowInstanceKey(workflowInstanceKey)
-                .withElementId("task")
+                .withElementId(ELEMENT_ID)
                 .limit(4))
         .hasSize(4)
         .extracting(Record::getIntent)
@@ -189,33 +285,27 @@ public class MultiInstanceTest {
             WorkflowInstanceIntent.ELEMENT_COMPLETED);
 
     assertThat(
-            RecordingExporter.workflowInstanceRecords(WorkflowInstanceIntent.ELEMENT_COMPLETED)
-                .withRecordKey(workflowInstanceKey)
-                .limitToWorkflowInstanceCompleted()
-                .exists())
-        .isTrue();
+            RecordingExporter.workflowInstanceRecords()
+                .filterRootScope()
+                .limitToWorkflowInstanceCompleted())
+        .extracting(Record::getIntent)
+        .contains(WorkflowInstanceIntent.ELEMENT_COMPLETED);
   }
 
   @Test
   public void shouldIgnoreInputElementVariableIfNotDefined() {
+    // given
     ENGINE
         .deployment()
-        .withXmlResource(
-            Bpmn.createExecutableProcess(PROCESS_ID)
-                .startEvent()
-                .serviceTask(
-                    "task",
-                    t ->
-                        t.zeebeTaskType("test").multiInstance(m -> m.zeebeInputCollection("items")))
-                .endEvent()
-                .done())
+        .withXmlResource(workflow(m -> m.zeebeInputCollection(INPUT_COLLECTION)))
         .deploy();
 
+    // when
     final long workflowInstanceKey =
         ENGINE
             .workflowInstance()
             .ofBpmnProcessId(PROCESS_ID)
-            .withVariable("items", Arrays.asList("a", "b", "c"))
+            .withVariable(INPUT_COLLECTION, Arrays.asList(10, 20, 30))
             .create();
 
     RecordingExporter.jobRecords(JobIntent.CREATED)
@@ -223,69 +313,112 @@ public class MultiInstanceTest {
         .limit(3)
         .exists();
 
-    ENGINE.jobs().withType("test").activate();
+    ENGINE.jobs().withType(JOB_TYPE).activate();
 
+    // then
     assertThat(
             RecordingExporter.jobRecords(JobIntent.ACTIVATED)
                 .withWorkflowInstanceKey(workflowInstanceKey)
                 .limit(3))
         .hasSize(3)
         .flatExtracting(r -> r.getValue().getVariables().keySet())
-        .containsOnly("items");
+        .containsOnly(INPUT_COLLECTION);
   }
 
   @Test
   public void shouldCreateIncidentIfInputVariableNotFound() {
-    ENGINE.deployment().withXmlResource(WORKFLOW_MI_PARALLEL_SERVICE_TASK).deploy();
+    // given
+    ENGINE.deployment().withXmlResource(WORKFLOW).deploy();
 
+    // when
     final long workflowInstanceKey = ENGINE.workflowInstance().ofBpmnProcessId(PROCESS_ID).create();
 
-    final Record<WorkflowInstanceRecordValue> elementActivating =
-        RecordingExporter.workflowInstanceRecords(WorkflowInstanceIntent.ELEMENT_ACTIVATING)
-            .withWorkflowInstanceKey(workflowInstanceKey)
-            .withElementId("task")
-            .getFirst();
-
-    final Record<IncidentRecordValue> incidentCreated =
+    // then
+    final Record<IncidentRecordValue> incident =
         RecordingExporter.incidentRecords(IncidentIntent.CREATED)
             .withWorkflowInstanceKey(workflowInstanceKey)
             .getFirst();
 
-    Assertions.assertThat(incidentCreated.getValue())
-        .hasElementInstanceKey(elementActivating.getKey())
-        .hasElementId(elementActivating.getValue().getElementId())
+    final Record<WorkflowInstanceRecordValue> elementInstance =
+        RecordingExporter.workflowInstanceRecords(WorkflowInstanceIntent.ELEMENT_ACTIVATING)
+            .withWorkflowInstanceKey(workflowInstanceKey)
+            .withElementId(ELEMENT_ID)
+            .getFirst();
+
+    Assertions.assertThat(incident.getValue())
+        .hasElementInstanceKey(elementInstance.getKey())
+        .hasElementId(elementInstance.getValue().getElementId())
         .hasErrorType(ErrorType.EXTRACT_VALUE_ERROR)
         .hasErrorMessage(
-            "Expected multi-instance input collection variable 'items' to be an ARRAY, but not found.");
+            "Expected multi-instance input collection variable '"
+                + INPUT_COLLECTION
+                + "' to be an ARRAY, but not found.");
+
+    // when resolve incident
+    ENGINE
+        .variables()
+        .ofScope(incident.getValue().getVariableScopeKey())
+        .withDocument(Collections.singletonMap(INPUT_COLLECTION, Arrays.asList(1, 2, 3)))
+        .update();
+
+    ENGINE.incident().ofInstance(workflowInstanceKey).withKey(incident.getKey()).resolve();
+
+    assertThat(
+            RecordingExporter.workflowInstanceRecords()
+                .withRecordKey(elementInstance.getKey())
+                .limit(2))
+        .extracting(Record::getIntent)
+        .contains(WorkflowInstanceIntent.ELEMENT_ACTIVATED);
   }
 
   @Test
   public void shouldCreateIncidentIfInputVariableIsNotAnArray() {
-    ENGINE.deployment().withXmlResource(WORKFLOW_MI_PARALLEL_SERVICE_TASK).deploy();
+    // given
+    ENGINE.deployment().withXmlResource(WORKFLOW).deploy();
 
+    // when
     final long workflowInstanceKey =
         ENGINE
             .workflowInstance()
             .ofBpmnProcessId(PROCESS_ID)
-            .withVariable("items", "1,2,3")
+            .withVariable(INPUT_COLLECTION, "no-an-array")
             .create();
 
-    final Record<WorkflowInstanceRecordValue> elementActivating =
-        RecordingExporter.workflowInstanceRecords(WorkflowInstanceIntent.ELEMENT_ACTIVATING)
-            .withWorkflowInstanceKey(workflowInstanceKey)
-            .withElementId("task")
-            .getFirst();
-
-    final Record<IncidentRecordValue> incidentCreated =
+    // then
+    final Record<IncidentRecordValue> incident =
         RecordingExporter.incidentRecords(IncidentIntent.CREATED)
             .withWorkflowInstanceKey(workflowInstanceKey)
             .getFirst();
 
-    Assertions.assertThat(incidentCreated.getValue())
-        .hasElementInstanceKey(elementActivating.getKey())
-        .hasVariableScopeKey(workflowInstanceKey)
+    final Record<WorkflowInstanceRecordValue> elementInstance =
+        RecordingExporter.workflowInstanceRecords(WorkflowInstanceIntent.ELEMENT_ACTIVATING)
+            .withWorkflowInstanceKey(workflowInstanceKey)
+            .withElementId(ELEMENT_ID)
+            .getFirst();
+
+    Assertions.assertThat(incident.getValue())
+        .hasElementInstanceKey(elementInstance.getKey())
+        .hasElementId(elementInstance.getValue().getElementId())
         .hasErrorType(ErrorType.EXTRACT_VALUE_ERROR)
         .hasErrorMessage(
-            "Expected multi-instance input collection variable 'items' to be an ARRAY, but found 'STRING'.");
+            "Expected multi-instance input collection variable '"
+                + INPUT_COLLECTION
+                + "' to be an ARRAY, but found 'STRING'.");
+
+    // when resolve incident
+    ENGINE
+        .variables()
+        .ofScope(incident.getValue().getVariableScopeKey())
+        .withDocument(Collections.singletonMap(INPUT_COLLECTION, Arrays.asList(1, 2, 3)))
+        .update();
+
+    ENGINE.incident().ofInstance(workflowInstanceKey).withKey(incident.getKey()).resolve();
+
+    assertThat(
+            RecordingExporter.workflowInstanceRecords()
+                .withRecordKey(elementInstance.getKey())
+                .limit(2))
+        .extracting(Record::getIntent)
+        .contains(WorkflowInstanceIntent.ELEMENT_ACTIVATED);
   }
 }
